81f00eb701ca91dcbd647ff0cb5137e8f9bb0117
David Balatero <dbalatero@gmail.com>
2017-12-01 20:36:16 -0800

    Use a sorted list

--- postinstall.sh
+++ postinstall.sh
@@ -1,13 +1,46 @@
 #!/bin/sh
 
+node_libraries=(
+  assert
+  buffer
+  console
+  constants
+  crypto
+  dns
+  domain
+  events
+  fs
+  http
+  https
+  inherits
+  net
+  os
+  path
+  process
+  punycode
+  querystring
+  stream
+  string_decoder
+  timers
+  tty
+  url
+  util
+  vm
+  zlib
+)
+
+nodeify_list="${node_libraries[0]}$(printf ",%s" "${node_libraries[@]:1}")"
+
 # rn-nodeify
 # temporary hack due to https://github.com/facebook/react-native/issues/4968
-./node_modules/.bin/rn-nodeify --yarn --install "assert,zlib,buffer,inherits,console,constants,crypto,dns,domain,events,http,https,os,path,process,punycode,querystring,fs,stream,string_decoder,timers,tty,url,util,net,vm" --hack;
-# npm install --save react@16.0.0-alpha.6;
-# npm install --save react-native-tcp@3.2.1;
+./node_modules/.bin/rn-nodeify \
+  --yarn \
+  --hack \
+  --install "$nodeify_list"
+
 ln -s ../node_modules/react-native-mapbox-gl/ios/Mapbox.framework ios;
 
-# # symlink for re-natal
+# symlink for re-natal
 if ! [ -f re-natal ]; then
   ln -s ./node_modules/re-natal/index.js re-natal
 else
